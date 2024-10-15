7ed67b1ce39cc1af22b4b4d42b3cafb7e1e09160
opilane <opilane@HP-CZC2349HTP>
2024-10-15 13:07:06 +0300

    not cool

---
+++ SuskeitsApi/Controllers/KasutajaController.cs
@@ -0,0 +1,21 @@
+﻿using SuskeitsApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuskeitsApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Kasutaja _kasutaja = new Kasutaja(1, "KoolaLover", "123ABC", "Vladlen","Semenov");
+
+        // GET: toode
+        [HttpGet]
+        public Kasutaja GetKasutaja()
+        {
+            return _kasutaja;
+        }
+
+    }
+}
